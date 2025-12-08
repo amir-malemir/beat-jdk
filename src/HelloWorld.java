f384492348dc39502feb401beb1b2bd5934b4900
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-08 09:17:25 +0330

    learn 'and' 'or'

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.print("Hello world");
-
+//        and or
         boolean is_active = false;
         if (is_active == false) {
             System.out.print("active");
