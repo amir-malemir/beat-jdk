0d62d75a44d87f8b06ee01b8f2f77fb144146ff8
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-08 10:05:00 +0330

    challenge for numbers and operators

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,23 +1,28 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.print("Hello world");
-//        and or
-        boolean is_active = false;
-        if (is_active == false) {
-            System.out.print("active");
-        int topscore = 99;
+//        System.out.print("Hello world");
+////        and or
+//        boolean is_active = false;
+//        if (is_active == false) {
+//            System.out.print("active");
+//        int topscore = 99;
+//
+//        if (topscore < 100 && is_active == true){
+//            System.out.println("smaller than topscore");
+//        }
+//        if (topscore > 100 || !is_active){
+//            System.out.println("smaller than topscore");
+//        }
+//        String makeFun = "amir";
+//        boolean isPerson = makeFun == "amir" ? false : true;
+//        System.out.print("isPerson: " + isPerson);
+          double numberOne = 20.00;
+          double numberTwo = 80.00;
+          double multyTwoNumber = numberOne + numberTwo * 100.00;
 
-        if (topscore < 100 && is_active == true){
-            System.out.println("smaller than topscore");
-        }
-        if (topscore > 100 || !is_active){
-            System.out.println("smaller than topscore");
-        }
-        String makeFun = "amir";
-        boolean isPerson = makeFun == "amir" ? false : true;
-        System.out.print("isPerson: " + isPerson);
-
+          if (multyTwoNumber == 0.00) {
+             System.out.println("True");
         }
     }
 }
