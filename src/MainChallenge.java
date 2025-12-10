62416d52c3b421ce43a7379994f160e627161711
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-10 10:18:52 +0330

    train for learn methods - int

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,29 +1,34 @@
 public class MainChallenge {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         boolean gameOver = true;
         int score = 8000;
         int levelCompleted = 8;
         int bonus = 200;
 
+        int highScore = CalcScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("your first round" + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        System.out.println("your next score" +
+                CalcScore(gameOver, score, levelCompleted, bonus));
+
+    }
+
+    public static int CalcScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         int finalScore = score;
 
-        CalcScore(true, 500, 12, 20);
-        if (gameOver){
+
+        if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score is " + finalScore);
+            return finalScore;
         }
+
+        return finalScore;
     }
-    public static void CalcScore( boolean gameOver, int score, int levelCompleted, int bonus ) {
-
-        int finalScore = score;
-
-
-        if (gameOver){
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score from method is " + finalScore);
-        }
-    }
-        }
+}
 
 
