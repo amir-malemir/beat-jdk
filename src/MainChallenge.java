31167d62fec14f306c0bdd70ea335af7f4092889
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-15 09:39:36 +0330

    create train class

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -57,6 +57,15 @@ public class MainChallenge {
         return position;
 
     }
+    public static void PosNegZero(int number){
+        if (number < 0) {
+            System.out.println("negative");
+        } else if (number > 0){
+            System.out.println("Positive");
+        } else {
+            System.out.println("Zero");
+        }
+    }
 }
 
 
