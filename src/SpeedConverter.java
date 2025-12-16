a38c1e011e2dc52e41e8c91e72e72fa5cbebdef1
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-16 20:40:34 +0330

    train 2 - fix problems

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,4 +1,13 @@
 public class SpeedConverter {
+    public static void main(String[] args){
 
-
+    convertToMile(2);
+    }
+    public static double convertToMile(double kilometers){
+        if (kilometers < 0){
+            return -1;
+        }
+        double miles = kilometers * 1.609344;
+        return miles;
+    }
 }
