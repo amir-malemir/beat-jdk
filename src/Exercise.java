47bb1dd805891151729b0679f79b806a94fa08e1
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-17 18:45:35 +0330

    train 3 - Create conveter method

--- src/Exercise.java
+++ src/Exercise.java
@@ -2,5 +2,12 @@ public class Exercise {
     public static void main(String[] args){
         mbConverter(10);
     }
+    public static long mbConverter(float mb){
 
+        if (mb < 0){
+            return -1;
+        }
+        int megaBytes
+
+    }
 }
