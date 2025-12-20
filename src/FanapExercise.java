059b745fad737b72400e49c3946fd54bdfe9985a
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-20 07:36:34 +0330

    train 6 - Equal Sum Checker

--- src/FanapExercise.java
+++ src/FanapExercise.java
@@ -1,11 +1,19 @@
 public class FanapExercise {
     public static void main(String[] args){
-        System.out.println(decimalPlace(-3.175,3.176));
+//        System.out.println(decimalPlace(-3.175,3.176));
+        System.out.println("output: " + equalSumChecker(0, 1, 2));
     }
-    public static boolean decimalPlace(double first, double second){
-        long firstRounded = (long) first * 1000;
-        long secondRounded = (long) second * 1000;
-        return firstRounded == secondRounded;
+//    public static boolean decimalPlace(double first, double second){
+//        long firstRounded = (long) first * 1000;
+//        long secondRounded = (long) second * 1000;
+//        return firstRounded == secondRounded;
+//    }
+    public static boolean equalSumChecker(int first, int second, int value){
+        int sumNumbers = first + second;
+        if (sumNumbers == value) {
+            return true;
+        }
+        return false;
     }
 
 
