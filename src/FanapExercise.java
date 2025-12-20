79b6576db0ce667797864195d854fbb3e436391a
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-20 19:06:53 +0330

    minutes and second challenge

--- src/FanapExercise.java
+++ src/FanapExercise.java
@@ -3,8 +3,10 @@ public class FanapExercise {
 //        System.out.println(decimalPlace(-3.175,3.176));
 //        System.out.println("output: " + equalSumChecker(0, 1, 2));
 //        System.out.println("out: " + teenNumberChecker(1,20,27) );
-        System.out.println("5ft, 8in = " + convertCM(5,8) + "cm");
-        System.out.println("68in = " + convertCM(68) + "cm");
+//        System.out.println("5ft, 8in = " + convertCM(5,8) + "cm");
+//        System.out.println("68in = " + convertCM(68) + "cm");
+        System.out.println("min is: " + convertMin(3699));
+        System.out.println("min and sec is: " + convertMin(2, 5));
     }
 //    public static boolean decimalPlace(double first, double second){
 //        long firstRounded = (long) first * 1000;
@@ -23,12 +25,27 @@ public class FanapExercise {
 //    public static boolean teenNumberChecker(int first, int second, int value){
 //        return first <= value && value <= second;
 //    }
-    public static double convertCM(int inch){
-        return inch * 2.54;
-    }
-    public static double convertCM(int feet, int inch){
-        return ((feet * 12) + inch) * 2.54;
-    }
+//    public static double convertCM(int inch){
+//        return inch * 2.54;
+//    }
+//    public static double convertCM(int feet, int inch){
+//        return ((feet * 12) + inch) * 2.54;
+//    }
 
+    public static String convertMin(int second){
+        int minutes = second / 60;
+        int hours = minutes / 60;
+        System.out.println("hours: " + hours );
+
+        int remainingMinutes = minutes % 60;
+        System.out.println("remaining minutes: " + remainingMinutes );
+
+        int remainingSeconds = second % 60;
+        System.out.println("remaining seconds: " + remainingSeconds );
+        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s" ;
+    }
+    public static int convertMin(int min, int sec){
+        return min/60 * sec;
+    }
 
 }
