e140858ae02e16ba1b480defa7aaca8e0cc498a3
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-12-27 07:14:10 +0330

    create Equality Printer

--- src/FanapExercise.java
+++ src/FanapExercise.java
@@ -1,4 +1,9 @@
 public class FanapExercise {
+//    private static final int MIN_PER_HOUR = 60;
+//    private static final int HOURS_PER_DAY = 24;
+//    private static final int DAYS_PER_YEAR = 365;
+//    private static final int MIN_PER_DAYS = HOURS_PER_DAY * MIN_PER_HOUR;
+//    private static final int MIN_PER_YEAR = MIN_PER_DAYS * HOURS_PER_DAY * DAYS_PER_YEAR;
     public static void main(String[] args){
 //        System.out.println(decimalPlace(-3.175,3.176));
 //        System.out.println("output: " + equalSumChecker(0, 1, 2));
@@ -7,6 +12,8 @@ public class FanapExercise {
 //        System.out.println("68in = " + convertCM(68) + "cm");
 //        System.out.println("time is: " + convertMin(3945));
 //        System.out.println("time is: " + convertMin(65, 45));
+//        System.out.println("area calc is : " + area(5.0, 4.0));
+
     }
 //    public static boolean decimalPlace(double first, double second){
 //        long firstRounded = (long) first * 1000;
@@ -52,18 +59,31 @@ public class FanapExercise {
 //        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s" ;
 //    }
 
-    private static final int invalid_value = -1;
+//    private static final int invalid_value = -1;
+//
+//    public static double area(double radius){
+//        if (radius < 0){
+//            return invalid_value;
+//        }
+//        return radius * radius * Math.PI;
+//    }
+//    public static double area(double x, double y){
+//        if (x < 0 || y < 0){
+//            return  invalid_value;
+//        }
+//        return x * y;
+//    }
+//    public static void printYearAndDays(long minutes){
+//        if (minutes < 0){
+//            System.out.println("Invalid Value");
+//            return;
+//        }
+//        long years = minutes / MIN_PER_YEAR;
+//        long days = (minutes / MIN_PER_DAYS) % DAYS_PER_YEAR;
+//        System.out.println(minutes + " min = " + years + "y and " + days + " d ");
+//    }
 
-    public static double area(double radius){
-        if (radius < 0){
-            return invalid_value;
-        }
-        return radius * radius * Math.PI;
-    }
-    public static double area(double x, double y){
-        if (x < 0 || y < 0){
-            return  invalid_value;
-        }
-        return x * y;
+    public static void eqaulityPrinter(int number1, int number2, int number3){
+
     }
 }
