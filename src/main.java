b182c803f7ebd7f831c1711c3038ba1a276fa30c
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-05 07:39:54 +0330

    convert if to switch

--- src/main.java
+++ src/main.java
@@ -1,12 +1,21 @@
 public class main {
     public static void main(String[] args){
         int value = 2;
-        if (value == 1){
-            System.out.println("value is 1");
-        } else if (value == 2) {
-            System.out.println("value is 2");
-        }else {
-            System.out.println("value is not 1 or 2");
+//        if (value == 1){
+//            System.out.println("value is 1");
+//        } else if (value == 2) {
+//            System.out.println("value is 2");
+//        }else {
+//            System.out.println("value is not 1 or 2");
+//        }
+        switch (value){
+            case 1:
+                System.out.println("was 1");
+            case 2:
+                System.out.println("was 2");
+            default:
+                System.out.println("was not 1 || 2");
         }
+
     }
 }
