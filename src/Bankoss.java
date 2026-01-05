79856580bc6ad84f401587f39dc0d79f78589c73
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-05 17:56:54 +0330

    create first step of user role

--- src/Bankoss.java
+++ src/Bankoss.java
@@ -1,5 +1,12 @@
 public class Bankoss {
     public static void main(String[] args){
+        System.out.println(approvalStatus("manager", "expired"));
+
+    }
+    public static String approvalStatus(String userRole, String status){
+        if (userRole == "manager"){
+            return "manager test";
+        }
 
     }
 }
