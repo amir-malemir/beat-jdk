f30525ce806cc2a2522ce918c2a70337fa8e5db2
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-05 07:34:09 +0330

    write if for exmp

--- src/main.java
+++ src/main.java
@@ -1,5 +1,12 @@
 public class main {
     public static void main(String[] args){
-        // more codes
+        int value = 2;
+        if (value == 1){
+            System.out.println("value is 1");
+        } else if (value == 2) {
+            System.out.println("value is 2");
+        }else {
+            System.out.println("value is not 1 or 2");
+        }
     }
 }
