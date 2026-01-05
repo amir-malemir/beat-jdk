52b701d7a3ab710c53ec9afaeff7be4ad7a6a585
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-05 19:53:28 +0330

    convert to interface

--- src/Bankoss.java
+++ src/Bankoss.java
@@ -1,41 +1,60 @@
-public class Bankoss {
-    public static void main(String[] args){
-        approvalStatus(2, "NOT_CONFIRM", 55);
+interface requestStatus{
+    public void clientdata();
+    public void databaseField();
+    public String clientCheckler();
+    public void approvalStatus();
+    }
+
+    static class clientReq implements requestStatus{
+        public void clientdata() {
+            approvalStatus(2, "CONFIRM", 55);
 //        status --> "CONFIRM" / "NOT_CONFIRM" / "EXPIRED" / "REJECT"
 //        userRole --> 1 (admin) / --> 2 (client)
-
-    }
-    public static void databaseField(String newStatus,int newUserRole,int newDate){
-        String status = newStatus;
-        int userRole = newUserRole;
-        int date = newDate;
-        System.out.println("database field updated. (" + "status: " + status + " , user role: " + userRole + " , date: " + date + ")");
-    }
-    public static void approvalStatus(int userRole, String status, int date){
-        int today = 10;
-        String statusValue = status;
-        if (userRole == 1){
-             if(statusValue == "NOT_CONFIRM" && date > today){
-                System.out.println("request is expired");
-            }
-            else if(statusValue == "EXPIRED" || date > today) {
-                System.out.println("expired, pls check date");
-            }else if (statusValue == "CONFIRM"){
-                System.out.println("ticket completed");
-            }
-            else if (statusValue == "REJECT"){
-                System.out.println("request rejected");
-            }else {
-                System.out.println("request needs to confirm");
-            }
-        }else if (userRole == 2){
-            if (statusValue == "NOT_CONFIRM" && date > today){
-                databaseField("EXPIRED", userRole, date);
-                System.out.println("request is expired");
-            } else if (statusValue == "EXPIRED") {
-                System.out.println("request is expired");
-            }
         }
 
+        public void databaseField(String newStatus,int newUserRole,int newDate){
+            String status = newStatus;
+            int userRole = newUserRole;
+            int date = newDate;
+            System.out.println("database field updated. (" + "status: " + status + " , user role: " + userRole + " , date: " + date + ")");
+        }
+        public String clientCheckler(boolean clientStatus){
+            return "your service approved but your status is expired for now :(, contact us";
+        }
+        public void approvalStatus(int userRole, String status, int date){
+            int today = 10;
+            String statusValue = status;
+            if (userRole == 1){
+                 if(statusValue == "NOT_CONFIRM" && date > today){
+                    System.out.println("request is expired");
+                }
+                else if(statusValue == "EXPIRED" || date > today) {
+                    System.out.println("expired, pls check date");
+                }else if (statusValue == "CONFIRM"){
+                    System.out.println("ticket completed");
+                }
+                else if (statusValue == "REJECT"){
+                    System.out.println("request rejected");
+                }else {
+                    System.out.println("request needs to confirm");
+                }
+            }else if (userRole == 2){
+                if (statusValue == "NOT_CONFIRM" && date > today){
+                    databaseField("EXPIRED", userRole, date);
+                    System.out.println("request is expired");
+                } else if (statusValue == "EXPIRED") {
+                    System.out.println("request is expired");
+                } else if (statusValue == "CONFIRM" && date > today) {
+                    System.out.println(clientCheckler(true));
+                }
+            }
+
+        }
     }
-}
+public static void main(String[] args){
+        clientReq myClient = new clientReq();
+        myClient.clientdata();
+
+
+    }
+
