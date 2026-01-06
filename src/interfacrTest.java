1c91121401b860c1871d706c326e3d0e9c1daeee
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-06 07:24:57 +0330

    refactoring

--- src/interfacrTest.java
+++ src/interfacrTest.java
@@ -1,33 +1,17 @@
 interface CheckRequest{
-    public void client(int userRole, String status, int date);
+    public void customerApporval(int userRole, String status, int date);
     public void database(int userRole, String status, int date);
     public String clientCheckler(boolean check);
 }
 class ReqStatus implements CheckRequest{
-    @Override
-    public void client(int userRole, String status, int date) {
+    public void  customerApporval(int userRole, String status, int date) {
         int today = 10;
-        if (userRole == 1){
-            if(status == "NOT_CONFIRM" && date > today){
-                System.out.println("request is expired");
-            }
-            else if(status == "EXPIRED" || date > today) {
-                System.out.println("expired, pls check date");
-            }else if (status == "CONFIRM"){
-                System.out.println("request sent, ticket completed");
-            }
-            else if (status == "REJECT"){
-                System.out.println("request rejected");
-            }else {
-                System.out.println("request needs to confirm");
-            }
-        }else if (userRole == 2){
-            if (status == "NOT_CONFIRM" && date > today){
-                database(userRole, status, date);
-            } else if (status == "EXPIRED") {
-                System.out.println("request is expired");
-            } else if (status == "CONFIRM" && date > today) {
-                System.out.println(clientCheckler(true));
+        if (date > today){
+            switch (userRole + status) {
+                case "2NOT_CONFIRM": database(userRole, status, date); break;
+                case "2EXPIRED": System.out.println("request is expired"); break;
+                case "2CONFIRM": System.out.println(clientCheckler(true));
+                default: System.out.println("no information");
             }
         }
     }
@@ -42,8 +26,8 @@ class ReqStatus implements CheckRequest{
 class Main{
     public static void main(String[] args) {
         ReqStatus myReqStatus = new ReqStatus();
-        myReqStatus.client(2, "CONFIRM", 50);
-        myReqStatus.client(1, "CONFIRM", 1);
+        myReqStatus.customerApporval(2, "NOT_CONFIRM", 50);
+        myReqStatus.client(1, "test", 10);
 
     }
 }
