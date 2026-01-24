2178c5c94282bda8d10e64d7081cf87714a99e99
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-24 20:04:46 +0330

    InstanceOf task - done + try&catch

--- src/InstanceOf.java
+++ src/InstanceOf.java
@@ -1,9 +1,27 @@
+import java.util.Objects;
 import java.util.logging.Logger;
 public class InstanceOf {
     private static final Logger logger = Logger.getLogger(InstanceOf.class.getName());
 
-    public static void main(String[] args) {
-        logger.info("Hello World");
 
+    public static void main(String[] args) {
+        processOutput("test");
+
+    }
+    public static void processOutput(String userInput){
+        Object obj = userInput;
+        try {
+            if (obj instanceof String){
+                logger.info("String");
+            }else if (obj instanceof Integer){
+                logger.info("Integer");
+            }else if (obj instanceof Double){
+                logger.info("Double");
+            }else if (obj instanceof Boolean){
+                logger.info("Boolean");
+            }
+        }catch (Exception e){
+            logger.warning(e.getMessage());
+        }
     }
 }
