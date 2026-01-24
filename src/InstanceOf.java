3845b40f2c20aaec507a6299d61872e3953d5cba
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2026-01-24 19:19:09 +0330

    InstanceOf task - create logger and test it

--- src/InstanceOf.java
+++ src/InstanceOf.java
@@ -1,2 +1,9 @@
+import java.util.logging.Logger;
 public class InstanceOf {
+    private static final Logger logger = Logger.getLogger(InstanceOf.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+
+    }
 }
